c7ea3b867c6f11ba3e3ac870941dd1c2bf5438b7
RobertSSoares <robertcomtmudoo@gmail.com>
2025-03-12 19:24:48 +0000

    Semi pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,27 +3,43 @@
 
 int main() {
 
-    int populacao, numero;
-    char estado[70];
-    char codigo[20];
-    char cidade[70];
+    int popu;
+    int turis;
+    char nome[30];
+    char esta[30];
+    char cida[30];
     float pib, area;
     
-    printf("Coloque o nome do estado:");
-    scanf("estado: %s\n", estado);
+    printf("Nome de um país:");
+    scanf("%[^\n]s", nome);
 
-    printf("Coloque o nome da cidade: \n");
-    scanf("cidade: %s\n", cidade);
-    printf("%s \n", cidade);
+    printf("Nome de um estado desse país:\n");
+    scanf("%[^\n]s", esta);
 
+    printf("Nome de uma cidade desse estado:\n");
+    scanf("%[^\n]s", cida);
 
+    printf("Coloque a população dessa cidade:\n");
+    scanf("%d", popu);
 
+    printf("O número de pontos turísticos:\n");
+    scanf("%d", turis);
 
+    printf("A área dessa cidade:\n");
+    scanf("%f", area);
 
+    printf("O PIB desse estado:\n");
+    scanf("%f", pib);
 
+    printf("País: %s\n", nome);
+    printf("Estado: %s\n", esta);
+    printf("Cidade: %s\n", cida);
 
+    printf("População: %d\n", popu);
+    printf("Pontos turísticos: %d\n", turis);
 
-
+    printf("Área da cidade: %f\n", area);
+    printf("PIB da cidade: %f\n", pib);
 
 
 
